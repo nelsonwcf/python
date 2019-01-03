7cd7bfb392eb061fc93c96b749724ba3b418c27a
Nelson Corrocher <grayman@gmail.com>
2019-01-03 00:33:29 -0600

    - still testing

---
+++ chapter_2_regression_scikit.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Chapter 2"""
+
+import os
+import tarfile
+import pandas as pd
+import matplotlib.pyplot as plt
+from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    """Download data for the project"""
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    """Load the housing data into pandas"""
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+fetch_housing_data()
+
+housing = load_housing_data()
+
+print(housing.head(), '\n')
+print(housing.columns, '\n')
+print(housing.info(), '\n')
+print(housing["ocean_proximity"].value_counts(), '\n')
+print(housing.describe(), '\n')
+
+# dataframe.hist() historgram of all numericals
+# check how to select matplotlib backend!
+housing.hist(bins=50, figsize=(20, 15))
