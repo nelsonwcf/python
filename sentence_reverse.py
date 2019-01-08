bc270dfe968d81c93613c7f97858ad6bd1740272
Nelson Corrocher <grayman@gmail.com>
2019-01-08 12:15:12 -0600

    test

--- sentence_reverse.py
+++ sentence_reverse.py
@@ -1,77 +1,24 @@
-arr = ['p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 
-       'm', 'a', 'k', 'e', 's', ' ', 'p', 'r',
-       'a', 'c', 't', 'i', 'c', 'e' ]
-
 def reverse_words(arr):
     if len(arr) <= 1:
         return arr
-    
-    mirror_(arr)
 
-    j = 0
-    for i in range(len(arr) + 1):
-        if i == len(arr) - 1 or arr[i] == ' ':
-            mirror_(arr, j, i - 1)
-            j = i + 1
+    # in-place reversal
+    mirror_(arr, 0, len(arr) - 1)
+
+    start_position = 0
+    for i in range(len(arr)):
+        if arr[i] == ' ':
+            mirror_(arr, start_position, i - 1)
+            start_position = i + 1
+
+    mirror_(arr, start_position, len(arr) - 1)
     
     return arr
 
-def mirror_(arr, i=0, j=len(arr) - 1):
+def mirror_(arr, i, j):
     while i <= j:
         c = arr[i]
         arr[i] = arr[j]
         arr[j] = c
         i += 1
         j -= 1
-
-reverse_words(arr)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def reverse_words(arr):
-#  if arr is None or len(arr) == 1:
-#    return arr
-#    
-#  _mirror(arr, 0, len(arr) - 1)
-#    
-#  start_position = 0
-#    
-#  for i in range(len(arr)):
-#      if arr[i] is ' ':
-#          end_position = i - 1
-#          _mirror(arr, start_position, end_position)
-#          start_position = i + 1
-#            
-#  _mirror(arr, start_position, len(arr) - 1)    
-#    
-#  return arr
-#
-#def _mirror(arr, start, end):
-#  if start < end:
-#      for i in range(1+(end - start)//2):
-#          tmp = arr[start + i]
-#          arr[start + i] = arr[end - i]
-#          arr[end - i] = tmp
